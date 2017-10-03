4191b6a1e83453cf904fac70db0d6eb830f2dd94
Alexey Lyanguzov <budabum@gmail.com>
2017-10-03 23:33:39 +0300

    way to implement calc

--- course2/calculator/src/main/scala/calculator/Calculator.scala
+++ course2/calculator/src/main/scala/calculator/Calculator.scala
@@ -1,21 +1,41 @@
 package calculator
 
-sealed abstract class Expr
-final case class Literal(v: Double) extends Expr
-final case class Ref(name: String) extends Expr
-final case class Plus(a: Expr, b: Expr) extends Expr
-final case class Minus(a: Expr, b: Expr) extends Expr
-final case class Times(a: Expr, b: Expr) extends Expr
-final case class Divide(a: Expr, b: Expr) extends Expr
+sealed abstract class Expr {
+  def value(): Double
+}
+
+final case class Literal(v: Double) extends Expr {
+  override def value(): Double = v
+}
+
+final case class Ref(name: String) extends Expr {
+  override def value(): Double = 77
+}
+
+final case class Plus(a: Expr, b: Expr) extends Expr {
+  override def value(): Double = 88
+}
+
+final case class Minus(a: Expr, b: Expr) extends Expr {
+  override def value(): Double = 66
+}
+
+final case class Times(a: Expr, b: Expr) extends Expr {
+  override def value(): Double = 44
+}
+
+final case class Divide(a: Expr, b: Expr) extends Expr {
+  override def value(): Double = 22
+}
 
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.mapValues(e => Signal(eval(e.apply(), Map())))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr.value()
   }
 
   /** Get the Expr for a referenced variables.
--- course2/calculator/src/main/scala/calculator/Polynomial.scala
+++ course2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,17 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    def disc = b() * b() - 4 * a() * c()
+    Signal(disc)
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal(
+      Set(
+        (-b() - Math.sqrt(delta())) / 2 * a(),
+        (-b() + Math.sqrt(delta())) / 2 * a()
+      )
+    )
   }
 }
