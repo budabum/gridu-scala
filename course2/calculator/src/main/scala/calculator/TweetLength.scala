f74d501116a394ff6308ab7eb5d35521bc63c126
Alexey Lyanguzov <budabum@gmail.com>
2017-10-03 22:32:30 +0300

    2week4: tweetlength is ready

---
+++ course2/calculator/src/main/scala/calculator/TweetLength.scala
@@ -0,0 +1,43 @@
+package calculator
+
+object TweetLength extends App {
+  final val MaxTweetLength = 140
+
+  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
+    def num = MaxTweetLength - tweetLength(tweetText())
+    Signal(num)
+  }
+
+  def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
+    def rc = remainingCharsCount()
+    Signal(
+      if(15 <= rc) "green"
+      else if(0 <= rc) "orange"
+      else "red"
+    )
+  }
+
+  /** Computes the length of a tweet, given its text string.
+   *  This is not equivalent to text.length, as tweet lengths count the number
+   *  of Unicode *code points* in the string.
+   *  Note that this is still a simplified view of the reality. Full details
+   *  can be found at
+   *  https://dev.twitter.com/overview/api/counting-characters
+   */
+  private def tweetLength(text: String): Int = {
+    /* This should be simply text.codePointCount(0, text.length), but it
+     * is not implemented in Scala.js 0.6.2.
+     */
+    if (text.isEmpty) 0
+    else {
+      text.length - text.init.zip(text.tail).count(
+          (Character.isSurrogatePair _).tupled)
+    }
+  }
+
+  val r = scala.util.Random
+  println(r.nextInt())
+  println(r.nextInt())
+  println(r.nextInt())
+  println(r.nextInt())
+}
