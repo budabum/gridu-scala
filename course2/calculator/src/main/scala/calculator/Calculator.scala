d03d614454da4d8ffae7585d97168d7c6375f8c2
Alexey Lyanguzov <budabum@gmail.com>
2017-10-04 08:11:04 +0300

    2week4: done by 8

--- course2/calculator/src/main/scala/calculator/Calculator.scala
+++ course2/calculator/src/main/scala/calculator/Calculator.scala
@@ -13,29 +13,37 @@ final case class Ref(name: String) extends Expr {
 }
 
 final case class Plus(a: Expr, b: Expr) extends Expr {
-  override def value(): Double = 88
+  override def value(): Double = a.value + b.value
 }
 
 final case class Minus(a: Expr, b: Expr) extends Expr {
-  override def value(): Double = 66
+  override def value(): Double = a.value - b.value
 }
 
 final case class Times(a: Expr, b: Expr) extends Expr {
-  override def value(): Double = 44
+  override def value(): Double = a.value * b.value
 }
 
 final case class Divide(a: Expr, b: Expr) extends Expr {
-  override def value(): Double = 22
+  override def value(): Double = a.value / b.value
 }
 
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    namedExpressions.mapValues(e => Signal(eval(e.apply(), Map())))
+    namedExpressions.mapValues(e => Signal(eval(e.apply(), namedExpressions)))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    expr.value()
+    expr match {
+      case Literal(x) => x
+      case Ref(v) => eval(getReferenceExpr(v, references), references)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+      case _ => 888
+    }
   }
 
   /** Get the Expr for a referenced variables.
