f72cd05166d0763f724427935f5c0bad544d0e84
Alexey Lyanguzov <budabum@gmail.com>
2018-01-02 19:02:35 +0300

    boxBlurKernel is ready

--- course3/scalashop/src/main/scala/scalashop/package.scala
+++ course3/scalashop/src/main/scala/scalashop/package.scala
@@ -1,6 +1,7 @@
 
 import common._
 
+import scala.collection.mutable.ListBuffer
 import scalashop.{Img, RGBA}
 
 package object scalashop {
@@ -39,17 +40,8 @@ package object scalashop {
     def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
   }
 
-  /** Computes the blurred RGBA value of a single pixel of the input image. */
-  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
-    // TODO implement using while loops
-    ???
-  }
-
-}
-
-object main extends App{
-  def rgbaArr(rgba: RGBA): List[Int] = {
-    List(
+  def rgbaArr(rgba: RGBA): Array[Int] = {
+    Array(
       scalashop.red(rgba),
       scalashop.green(rgba),
       scalashop.blue(rgba),
@@ -57,12 +49,32 @@ object main extends App{
     )
   }
 
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    val neighbors: ListBuffer[Array[Int]] = ListBuffer()
+    for {
+      y1 <- y-radius to y+radius
+      x1 <- x-radius to x+radius
+      if (0 <= x1 && x1 < src.width) && (0 <= y1 && y1 < src.height) //&& !(x1 == x && y1 == y)
+    } neighbors += rgbaArr(src.apply(x1, y1))
+    println(neighbors.map(n=>n.toList))
+    val rgbaMedian = neighbors.fold(Array(0,0,0,0))((n, a) =>
+      a.zip(n).map{case (i1, i2) => i1 + i2}).map(n => n / neighbors.size)
+    val rgbaMedianTuple = rgbaMedian match {case Array(a,b,c,d) => (a,b,c,d)}
+    rgba _ tupled rgbaMedianTuple
+  }
+
+}
+
+object main extends App{
+  import scalashop._
+
   def printImg(img: Img): Unit ={
     for {
-      x <- 0 until w
-      y <- 0 until h
-      s = if(y == h-1) "\n" else " "
-    } print(rgbaArr(img.apply(x, y)) + s)
+      y <- 0 until img.height
+      x <- 0 until img.width
+      s = if(x == img.width-1) "\n" else " "
+    } print(rgbaArr(img.apply(x, y)).toList + s)
 //    } print(x + ":" + y + "\n")
     println()
   }
@@ -74,10 +86,11 @@ object main extends App{
   for {
     x <- 0 until w
     y <- 0 until h
-    v = 32 * (x + 1) + y
-    c = scalashop.rgba(v, v, v, v)
+    v = y * w + x
+    c = rgba(v, v, v, v)
   } img.update(x, y, c)
   printImg(img)
+  println(rgbaArr(boxBlurKernel(img, 1, 2, 1)).toList)
 //  println(rgbaArr(0x10101010))
   println("FINISH")
 }
